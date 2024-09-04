64b7b17f444ee66959fcb01cfeb2e940d1cb303b
ua.lifesheets <ua.lifesheets@outlook.com>
2024-09-04 23:54:23 +0300

    Fix function for esc();

--- platform/functions/check.php
+++ platform/functions/check.php
@@ -1,9 +1,19 @@
 <?php
 
 # Абсолютный текст с фильтрацией без бб-кодов, смайлов и т.п.
-function tabs($text) {
+function tabs($text)
+{
   if (!is_string($text)) {
-      $text = '';
+    $text = '';
   }
   return stripslashes(htmlspecialchars($text, ENT_QUOTES, 'UTF-8'));
+}
+
+# Фильтрация текста перед записью в базу данных
+function esc($text)
+{
+  if (!is_string($text)) {
+    $text = '';
+  }
+  return addslashes($text);
 }
