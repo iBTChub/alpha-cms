edb9b425de2c573717529485b6e226632049a55a
ua.lifesheets <ua.lifesheets@icloud.com>
2024-09-05 10:09:50 +0300

    Fix function for clearspecialchars();

--- platform/functions/check.php
+++ platform/functions/check.php
@@ -1,5 +1,17 @@
 <?php
 
+# Полное удаление символов из текста без замены
+
+function clearspecialchars($text) {
+  $text = $text ?? '';
+  $special_chars = array('?', '[', ']', '/', '\\', '=', '<', '>', ':', ';', ',', "'", '"', '&', '$', '#', '*', '(', ')', '|', '~', '`', '!', '{', '}', '%', '+', chr(0));
+  $text = preg_replace("#\x{00a0}#siu", ' ', $text);
+  $text = str_replace($special_chars, '', $text);
+  $text = str_replace(array('%20', '+'), '', $text);
+  $text = trim($text, '.-_');	
+  return htmlspecialchars($text);
+}
+
 # Абсолютный текст с фильтрацией без бб-кодов, смайлов и т.п.
 function tabs($text)
 {
