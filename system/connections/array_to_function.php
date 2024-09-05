c42e9a2fea8dbe47f5c925a81e22b801d50585ea
ua.lifesheets <ua.lifesheets@outlook.com>
2024-09-05 15:30:05 +0300

    Fix function for b_icons();

--- system/connections/array_to_function.php
+++ system/connections/array_to_function.php
@@ -5,23 +5,24 @@
 Настройки пользователя
 ----------------------
 */
-  
-$settings_data = db::get_string("SELECT * FROM `USERS_SETTINGS` WHERE `USER_ID` = ? LIMIT 1", [user('ID')]); 
-  
-function settings($data){
-  
+
+$settings_data = db::get_string("SELECT * FROM `USERS_SETTINGS` WHERE `USER_ID` = ? LIMIT 1", [user('ID')]);
+
+function settings($data)
+{
+
   global $settings_data;
-  
-  if (user('ID') == 0){
-    
+
+  if (user('ID') == 0) {
+
     return 0;
-    
-  }else{
+
+  } else {
 
     return tabs($settings_data[$data]);
-    
+
   }
-  
+
 }
 
 /*
@@ -30,14 +31,15 @@ function settings($data){
 ---------------------------------------
 */
 
-function GenAvatar() {
-  
+function GenAvatar()
+{
+
   $avatar_rand_param = array('#6E8CCE', '#53C8A9', '#8FA1A9', '#FC8FD8', '#AFBC81', '#B381BC', '#EB6156', '#FD8B2C', '#72C375', '#B970C5', '#31ACB8', '#5498CE', '#997445', '#4EA771', '#828D92', '#F55448', '#2D88BE', '#E85120', '#333E42');
   $avatar_rand = array_rand($avatar_rand_param, 1);
   $avatar = $avatar_rand_param[$avatar_rand];
-  
+
   return $avatar;
-  
+
 }
 
 /*
@@ -46,18 +48,19 @@ function GenAvatar() {
 -------------------------------------------
 */
 
-if (!is_panel() && is_file(ROOT.'/style/version/'.version('DIR').'/includes/m_icons.php')) {
-  
-  require (ROOT.'/style/version/'.version('DIR').'/includes/m_icons.php');
+if (!is_panel() && is_file(ROOT . '/style/version/' . version('DIR') . '/includes/m_icons.php')) {
 
-}else{
-  
-  function icons($icons, $size = 15, $fw = null){
-    
-    return "<i style='font-size: ".$size."px; vertical-align: middle' class='fa fa-".$icons." ".$fw."'></i>";
-  
+  require(ROOT . '/style/version/' . version('DIR') . '/includes/m_icons.php');
+
+} else {
+
+  function icons($icons, $size = 15, $fw = null)
+  {
+
+    return "<i style='font-size: " . $size . "px; vertical-align: middle' class='fa fa-" . $icons . " " . $fw . "'></i>";
+
   }
-  
+
 }
 
 /*
@@ -66,16 +69,17 @@ if (!is_panel() && is_file(ROOT.'/style/version/'.version('DIR').'/includes/m_ic
 --------------------------------------
 */
 
-function file_icons($ext, $color = '#337AB7', $icons = 'file', $type = 'big'){
-  
+function file_icons($ext, $color = '#337AB7', $icons = 'file', $type = 'big')
+{
+
   if ($type == 'big') {
-    
-    return "<div class='corner-box' style='background: linear-gradient(to left bottom, transparent 50%, rgba(0,0,0,.4) 0) no-repeat 100% 0 / 15px 15px, linear-gradient(-135deg, transparent 10px, ".$color." 0);'><span class='corner-box-icons'>".icons($icons, 15)."</span><span class='corner-box-ext'>".$ext."</span></div>";
-    
-  }elseif ($type == 'small') {
-    
-    return "<span style='color: ".$color."'>".icons($icons, 17, 'fa-fw')."</span>";
-    
+
+    return "<div class='corner-box' style='background: linear-gradient(to left bottom, transparent 50%, rgba(0,0,0,.4) 0) no-repeat 100% 0 / 15px 15px, linear-gradient(-135deg, transparent 10px, " . $color . " 0);'><span class='corner-box-icons'>" . icons($icons, 15) . "</span><span class='corner-box-ext'>" . $ext . "</span></div>";
+
+  } elseif ($type == 'small') {
+
+    return "<span style='color: " . $color . "'>" . icons($icons, 17, 'fa-fw') . "</span>";
+
   }
 
 }
@@ -86,91 +90,72 @@ function file_icons($ext, $color = '#337AB7', $icons = 'file', $type = 'big'){
 --------------------
 */
 
-if (!is_panel()){
-  
-  if (is_file(ROOT.'/style/version/'.version('DIR').'/includes/icons.php')) {
-    
-    require (ROOT.'/style/version/'.version('DIR').'/includes/icons.php');
-  
-  }else{
-    
-    if (is_file(ROOT.'/style/version/'.version('DIR').'/includes/param_icons_mini.php')) {
-      
-      require (ROOT.'/style/version/'.version('DIR').'/includes/param_icons_mini.php');
-    
-    }else{
-      
+if (!is_panel()) {
+
+  if (is_file(ROOT . '/style/version/' . version('DIR') . '/includes/icons.php')) {
+
+    require(ROOT . '/style/version/' . version('DIR') . '/includes/icons.php');
+
+  } else {
+
+    if (is_file(ROOT . '/style/version/' . version('DIR') . '/includes/param_icons_mini.php')) {
+
+      require(ROOT . '/style/version/' . version('DIR') . '/includes/param_icons_mini.php');
+
+    } else {
+
       define('PARAM_ICONS_MINI_COLOR', null);
       define('PARAM_ICONS_MINI_BACKGROUND', null);
-    
+
     }
-  
-    function m_icons($icons, $size, $color = '#6E7B80', $i = 1){
-    
+
+    function m_icons($icons, $size, $color = '#6E7B80', $i = 1)
+    {
+
       if (str(PARAM_ICONS_MINI_COLOR) > 0 && str(PARAM_ICONS_MINI_BACKGROUND) > 0 && $i == 1) {
-      
+
         $color = PARAM_ICONS_MINI_BACKGROUND;
         $tcolor = PARAM_ICONS_MINI_COLOR;
-    
-      }else{
-      
+
+      } else {
+
         $color = $color;
         $tcolor = 'white';
-    
+
       }
-    
-      return "<span class='icons-circle' style='background-color: ".$color."'; color: ".$tcolor.">".icons($icons, $size)."</span>";
-  
+
+      return "<span class='icons-circle' style='background-color: " . $color . "'; color: " . $tcolor . ">" . icons($icons, $size) . "</span>";
+
     }
-  
-    if (is_file(ROOT.'/style/version/'.version('DIR').'/includes/param_icons.php')) {
-    
-      require_once (ROOT.'/style/version/'.version('DIR').'/includes/param_icons.php');
-  
-    }else{
-    
+
+    if (is_file(ROOT . '/style/version/' . version('DIR') . '/includes/param_icons.php')) {
+
+      require_once(ROOT . '/style/version/' . version('DIR') . '/includes/param_icons.php');
+
+    } else {
+
       define('PARAM_ICONS_COLOR', null);
       define('PARAM_ICONS_GH_BACKGROUND', null);
       define('PARAM_ICONS_GW_BACKGROUND', null);
       define('PARAM_COUNT_BACKGROUND', null);
-  
+
     }
-  
-    function b_icons($icons, $size, $count = null, $GHcolor = '#6E7B80', $GWcolor = '#6E7B80'){
+
+    function b_icons($icons, $count, $size, $GHcolor = '#6E7B80', $GWcolor = '#6E7B80')
+    {
       // Проверка на параметры и установка значений
       if (str(PARAM_ICONS_COLOR) > 0 && str(PARAM_ICONS_GW_BACKGROUND) > 0 && str(PARAM_ICONS_GH_BACKGROUND) > 0 && str(PARAM_COUNT_BACKGROUND) > 0) {
-          $ccolor = PARAM_COUNT_BACKGROUND;
-          $tcolor = PARAM_ICONS_COLOR;
-          $color = 'linear-gradient(to top left, '.PARAM_ICONS_GH_BACKGROUND.', '.PARAM_ICONS_GW_BACKGROUND.')';
+        $ccolor = PARAM_COUNT_BACKGROUND;
+        $tcolor = PARAM_ICONS_COLOR;
+        $color = 'linear-gradient(to top left, ' . PARAM_ICONS_GH_BACKGROUND . ', ' . PARAM_ICONS_GW_BACKGROUND . ')';
       } else {
-          $color = 'linear-gradient(to top left, '.$GHcolor.', '.$GWcolor.')';
-          $tcolor = 'white';
-          $ccolor = null;
+        $color = 'linear-gradient(to top left, ' . $GHcolor . ', ' . $GWcolor . ')';
+        $tcolor = 'white';
+        $ccolor = null;
       }
       // Формирование HTML-вывода
-      return "<div class='menu-container_icons' style='background: ".$color."; color: ".$tcolor."'><div style='".$ccolor."'>".$count."</div>".icons($icons, $size)."</div>";
+      return "<div class='menu-container_icons' style='background: " . $color . "; color: " . $tcolor . "'><div style='" . $ccolor . "'>" . $count . "</div>" . icons($icons, $size) . "</div>";
+    }
   }
 
-
-
-
-
-
-
-
-    // function b_icons($icons, $count = null, $size, $GHcolor = '#6E7B80', $GWcolor = '#6E7B80'){
-    //   if (str(PARAM_ICONS_COLOR) > 0 && str(PARAM_ICONS_GW_BACKGROUND) > 0 && str(PARAM_ICONS_GH_BACKGROUND) > 0 && str(PARAM_COUNT_BACKGROUND) > 0) {
-    //     $ccolor = PARAM_COUNT_BACKGROUND;
-    //     $tcolor = PARAM_ICONS_COLOR;
-    //     $color = 'linear-gradient(to top left, '.PARAM_ICONS_GH_BACKGROUND.', '.PARAM_ICONS_GW_BACKGROUND.')';
-    //   }else{
-    //     $color = 'linear-gradient(to top left, '.$GHcolor.', '.$GWcolor.')';
-    //     $tcolor = 'white';
-    //     $ccolor = null;
-    //   }
-    //   return "<div class='menu-container_icons' style='background: ".$color."; color: ".$tcolor."'><div style='".$ccolor."'>".$count."</div>".icons($icons, $size)."</div>";
-    // }
-    
-  }
-    
 }
