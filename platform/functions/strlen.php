989cae4b8d899835d3f6036913a98c1595fc1dd4
ua.lifesheets <ua.lifesheets@icloud.com>
2024-09-06 10:01:37 +0300

    Preparing packages to improve the project!

--- platform/functions/strlen.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-# Функция определения длины строки (str_length)
-function str($str)
-{
-  $str = sanitize_input($str);
-  return mb_strlen($str, 'UTF-8');
-}
---
+++ platform/functions/system.php
@@ -0,0 +1,7 @@
+<?php
+
+# Функция определения длины строки (str_length)
+function str($str) {
+    $str = sanitize_input($str);
+    return mb_strlen($str, 'UTF-8');
+}
