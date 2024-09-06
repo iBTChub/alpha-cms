dda76ccdadb85a2eaf3130c230188ac0fa19c2c1
ua.lifesheets <ua.lifesheets@icloud.com>
2024-09-06 17:07:50 +0300

    Web Page Sidebar Optimization

---
+++ languages.md
@@ -0,0 +1,23 @@
+# Transliteration for admin panel
+EN: Control panel | RU: Панель управления | UA: Панель керування
+EN: My page | RU: Моя страница | UA: Моя сторінка
+EN: Your account | RU: Ваш счет | UA: Ваш рахунок
+EN: Bonuses | RU: Бонусы | UA: Бонуси
+EN: Cabinet | RU: Кабинет | UA: Кабінет
+EN: Shop | RU: Магазин | UA: Магазин
+EN: Settings | RU: Настройка | UA: Налаштування
+EN: Exit | RU: Выход | UA: Вихід
+EN: IE | RU: ФОП | UA: ФОП
+EN: EDRPOU | RU: ЕГРПОУ | UA: ЄДРПОУ
+EN: RNNOVP | RU: РНОКПП | UA: РНОКПП
+EN: Dovhopol | RU: Довгопол | UA: Довгопол
+EN: Mykola | RU: Николай | UA: Микола
+EN: Ivanovich | RU: Иванович | UA: Іванович
+EN: Site sections | RU: Разделы сайта | UA: Розділи сайту
+EN: Authorization | RU: Авторизация | UA: Авторизація
+EN: Forgot your password? | RU: Забыли пароль? | UA: Забули свій пароль?
+EN: Registration | RU: Регистрация | UA: Реєстрація
+EN: Login | RU: Логин | UA: Логин
+EN: Password | RU: Пароль | UA: Пароль
+EN: Enter numbers | RU: Введите числа | UA: Введите числа
+EN: Login | RU: Войти | UA: Войти
--- style/version/web/includes/panel-left.php
+++ style/version/web/includes/panel-left.php
@@ -1,111 +1,139 @@
-<!-- Левая панель --!>
-  
-<div class="left">  
-  
-<div class="pfm"> 
-  
-<?php if (user('ID') > 0) { ?>  
-  
-<div class='panel-left-phone'>
-<div class='message cabinet-phone'>
-<div class='mess-circle1'></div> 
-<div class='mess-circle2'></div>
-</div>
-</div>
- 
-<center style='margin-bottom: 10px'>
-<div class='list-tr-avatar panel-left-avatar'>
-<a href='/id<?=user('ID')?>'><?=user::avatar(user('ID'), 80)?></a>
-</div>
-<div class='panel-left-login'><?=user::login_mini(user('ID'))?></div>
-  
-<div class='panel-left-menu-nav'>
-  
-<div class='cabinet-menu'>
-<a href='/account/cabinet/'><?=icons('th-large', 19)?></a>
-<span><?=lg('Кабинет')?></span>
-</div>
-
-<div class='cabinet-menu'>
-<a href='/shopping/'><?=icons('shopping-basket', 19)?></a>
-<span><?=lg('Магазин')?></span>
-</div>                              
-
-<div class='cabinet-menu'>
-<a href='/account/settings/'><?=icons('gear', 19)?></a>  
-<span><?=lg('Настройки')?></span>                             
-</div>
-
-<div class='cabinet-menu'>
-<a href='/exit/' ajax='no'><?=icons('power-off', 19)?></a> 
-<span><?=lg('Выход')?></span>                             
-</div>
-  
-</div>  
-</center>
-  
-<?php if (access('administration_show', null, 1) == true) { ?>
-<a href='/admin/' ajax='no' class='panel-left-menu pfm_gray hover'>
-<?=m_icons('gear', 13, '#2F454F', 0)?> <?=lg('Панель управления')?>
-</a> 
-<?php } ?>
-  
-<a class='panel-left-menu pfm_gray hover' href='/id<?=user('ID')?>'><?=m_icons('user', 11, '#2F454F', 0)?> <span><?=lg('Моя страница')?></span></a> 
-  
-<?php }else{ ?> 
-  
-<div class='panel-left-menu pfm_name'>
-<?=lg('Авторизация')?>
-</div>
-  
-<div class='panel-left-menu' style='box-sizing: border-box; border-bottom: 1px #EBF2F7 solid'>
-  
-<form method='post' action='/login/'>
-  
-<?=html::input('login', 'Логин', null, config('REG_STR'), null, 'form-control-100', 'text', null, 'user')?>
-<?=html::input('password', 'Пароль', null, 24, null, 'form-control-100', 'password', null, 'lock')?>
-  
 <?php
-  
-if (session('captcha') == 1 && url_request_validate('/login') == false && url_request_validate('/registration') == false && url_request_validate('/password') == false){
-  
-  html::captcha('Введите числа');
-  
+
+
+
+$date = date('Y');
+$host = tabs(HTTP_HOST);
+
+// Translate function
+$translate = fn($text) => lg($text);
+
+// Navigation icons
+$navGeneralMenuIcon = fn($icon, $size) => icons($icon, $size);
+$navDefaultMenuIcon = fn($icon, $size, $color, $checkColor) => m_icons($icon, $size, $color, $checkColor);
+
+// User information
+$userId = user('ID');
+$userAvatar = user::avatar($userId, 80);
+$userLogin = user::login_mini($userId);
+
+$userBalance = money(user('MONEY'), 1);
+$userBonuses = money(user('BALLS'), 1);
+
+// Admin panel link
+$showAdmin = access('administration_show', null, 1);
+$htmlLinkAccess = $showAdmin ? "<a href='/admin/' ajax='no' class='panel-left-menu pfm_gray hover'>{$navDefaultMenuIcon('gear', 13, '#2F454F', 0)} {$translate('Control panel')}</a>" : '';
+
+// Output HTML
+echo <<<HTML
+<div class="left">
+  <div class="pfm">
+HTML;
+
+// User-specific content
+if ($userId > 0) {
+    $userContent = <<<HTML
+    <div class='panel-left-phone'>
+        <div class='message cabinet-phone'>
+            <div class='mess-circle1'></div>
+            <div class='mess-circle2'></div>
+        </div>
+    </div>
+
+    <center style='margin-bottom: 10px'>
+        <div class='list-tr-avatar panel-left-avatar'>
+            <a href='/id{$userId}'>{$userAvatar}</a>
+        </div>
+
+        <div class='panel-left-login'>{$userLogin}</div>
+
+        {$translate('Your account')}: <b>{$userBalance}</b> {$translate('UAH')}.
+        <br />
+        {$translate('Bonuses')}: <b>{$userBonuses}</b> {$translate('UAH')}.
+
+        <div class='panel-left-menu-nav'>
+            <div class='cabinet-menu'>
+                <a href='/account/cabinet/'>{$navGeneralMenuIcon('th-large', 19)}</a>
+                <span>{$translate('Cabinet')}</span>
+            </div>
+
+            <div class='cabinet-menu'>
+                <a href='/shopping/'>{$navGeneralMenuIcon('shopping-basket', 19)}</a>
+                <span>{$translate('Shop')}</span>
+            </div>
+
+            <div class='cabinet-menu'>
+                <a href='/account/settings/'>{$navGeneralMenuIcon('gear', 19)}</a>
+                <span>{$translate('Settings')}</span>
+            </div>
+
+            <div class='cabinet-menu'>
+                <a href='/exit/' ajax='no'>{$navGeneralMenuIcon('power-off', 19)}</a>
+                <span>{$translate('Exit')}</span>
+            </div>
+        </div>
+    </center>
+    $htmlLinkAccess
+    <a class='panel-left-menu pfm_gray hover' href='/id{$userId}'>{$navDefaultMenuIcon('user', 11, '#2F454F', 0)} <span>{$translate('My page')}</span></a>
+HTML;
+} else {
+    // Authentication form for guests
+    ob_start();
+    html::input('login', $translate('Login'), null, config('REG_STR'), null, 'form-control-100', 'text', null, 'user');
+    html::input('password', $translate('Password'), null, 24, null, 'form-control-100', 'password', null, 'lock');
+    
+    if (session('captcha') == 1 && !url_request_validate('/login') && !url_request_validate('/registration') && !url_request_validate('/password')) {
+        html::captcha($translate('Enter numbers'));
+    }
+    
+    html::button('button', 'ok_aut', null, $translate('Login'));
+    $formHtml = ob_get_clean();
+    
+    $userContent = <<<HTML
+    <div class='panel-left-menu pfm_name'>
+      {$translate('Authorization')}
+    </div>
+
+    <div class='panel-left-menu' style='box-sizing: border-box; border-bottom: 1px #EBF2F7 solid'>
+      <form method='post' action='/login/'>
+        $formHtml
+        <br />
+        <a href='/password/' class='aut'>{$translate('Forgot your password?')}</a>
+        <a href='/registration/' style='float: right;' class='aut'>{$translate('Registration')}</a>
+      </form>
+    </div>
+HTML;
 }
 
-html::button('button', 'ok_aut', null, 'Войти');  
-?>
-  
-<br />
-  
-<a href='/password/' class='aut'><?=lg('Забыли пароль?')?></a>
-<a href='/registration/' style='float: right;' class='aut'><?=lg('Регистрация')?></a>
-  
-</form>
-  
-</div>
-  
-<?php } ?>
-  
-<div class='panel-left-menu pfm_name'>
-<?=lg('Разделы сайта')?>
-</div>
-  
-<?=direct::components(ROOT.'/main/components/main_menu_small/', 0)?>
+// Output user content or authentication form
+echo $userContent;
 
-<button class='panel-left-menu pfm_gray hover' onclick="modal_center('version', 'open', '/system/AJAX/php/version.php', 'ver_upload')"><?=m_icons('desktop', 11, '#2F454F', 0)?> <span><?=lg('Версия')?>: <?=VERSION?></span></button>
-  
-<button class='panel-left-menu pfm_gray hover' onclick="modal_center('languages', 'open', '/system/AJAX/php/languages.php', 'lang_upload')"><?=m_icons('globe', 12, '#2F454F', 0)?> <span><?=lg('Язык')?>: <?=LANGUAGE?></span></button>
-  
-<div class='panel-left-menu'>
-<center>
-<span class='time'>
-© <?=tabs(HTTP_HOST)?> - <?=date('Y')?>
-</span>
-</center>
+echo <<<HTML
+    <!-- Site sections -->
+    <div class='panel-left-menu pfm_name'>
+      {$translate('Site sections')}
+    </div>
+HTML;
+
+direct::components(ROOT . '/main/components/main_menu_small/', 0);
+
+echo <<<HTML
+      <!-- Footer content -->
+      <div class='panel-left-menu'>
+        <center>
+          <span class='time'>
+            © $date - {$translate('Interactive Brand & Technologies: Courses, Services & Business Hub')}
+            <br />
+            <br />
+            {$translate('IE')} {$translate('Dovhopol')} {$translate('Mykola')} {$translate('Ivanovich')}
+            <br />
+            {$translate('EDRPOU')} / {$translate('RNNOVP')} 3484109139
+            <br />
+          </span>
+        </center>
+      </div>
+    <div class='panel-top-optimize3'></div>
+  </div>
 </div>
-  
-<div class='panel-top-optimize3'></div>
-  
-</div>
-</div>
+HTML;
+?>
